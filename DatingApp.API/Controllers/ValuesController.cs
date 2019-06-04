38bdca0b06957db7850d23879dab6afbaad656a6
Kirsty Boyle <Kirsty.Boyle@graham.co.uk>
2019-06-04 13:52:19 +0100

    first commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context) //this allows you to access the Datacontext which holds the values
+        {
+            _context = context; 
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues() 
+        {
+           var values = await _context.Values.ToListAsync();
+
+           return Ok(values);
+        } 
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);//picks up the default value in none are specificed
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
